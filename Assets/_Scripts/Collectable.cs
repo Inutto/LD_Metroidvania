daa3176353c1bd115830ff092627d419e1b1ca79
Inutto <57230511+Inutto@users.noreply.github.com>
2022-03-23 17:38:53 -0600

    Update Pickup Impl

---
+++ Assets/_Scripts/Collectable.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MoreMountains.CorgiEngine;
+
+
+
+
+public class Collectable : MonoBehaviour
+{
+
+    public int points;
+    private Animator _anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if(collision.tag == "Player")
+        {
+            GameManager.Instance.AddPoints(points);
+            GUIManager.Instance.RefreshPoints();
+
+            _anim.SetTrigger("Fade");
+            
+
+        }
+
+    }
+
+    public void EndFade()
+    {
+        Destroy(gameObject);
+    }
+
+}
