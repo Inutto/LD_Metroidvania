2950daebdc197ee79710844587fdcd1df6d3026d
Inutto <57230511+Inutto@users.noreply.github.com>
2022-03-27 00:44:24 -0600

    Update Pickup Animations

--- Assets/_Scripts/Collectable.cs
+++ Assets/_Scripts/Collectable.cs
@@ -6,16 +6,40 @@ using MoreMountains.CorgiEngine;
 
 
 
+
 public class Collectable : MonoBehaviour
 {
 
     public int points;
     private Animator _anim;
+    public float bounceOffset = 1f;
+    public float bounceInterval = 1f;
 
     // Start is called before the first frame update
     void Start()
     {
         _anim = GetComponent<Animator>();
+        // LeanTween.moveY(gameObject, 1, 1).setEase(LeanTweenType.easeInBounce);
+        StartCoroutine(BounceUp());
+        
+
+    }
+
+
+    public IEnumerator BounceUp()
+    {
+        LeanTween.moveY(gameObject, transform.position.y + bounceOffset, bounceInterval)
+            .setEase(LeanTweenType.easeInOutSine);
+        yield return new WaitForSeconds(bounceInterval);
+        StartCoroutine(BounceDown());
+    }
+
+    public IEnumerator BounceDown()
+    {
+        LeanTween.moveY(gameObject, transform.position.y - bounceOffset, bounceInterval)
+            .setEase(LeanTweenType.easeInOutSine);
+        yield return new WaitForSeconds(bounceInterval);
+        StartCoroutine(BounceUp());
     }
 
     // Update is called once per frame
