442b8a9620bc1a8d0f6fe8d08bed37bc4ee425c5
Inutto <57230511+Inutto@users.noreply.github.com>
2022-03-27 11:31:44 -0600

    Fixed the UI Bug

--- Assets/_Scripts/PlayerHealthMask.cs
+++ Assets/_Scripts/PlayerHealthMask.cs
@@ -8,38 +8,64 @@ public class PlayerHealthMask : MonoBehaviour
 {
 
     public CanvasGroup mainGroup;
-    public Character player;
+    public Character character;
+    public Text text;
 
     public bool canFind = false;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        mainGroup = GetComponent<CanvasGroup>();
+        
         StartCoroutine(WaitForFindingPlayer());
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
         if (canFind)
         {
-            float health = player.CharacterHealth.CurrentHealth;
-            float maxHealth = player.CharacterHealth.MaximumHealth;
+            float health = character.CharacterHealth.CurrentHealth;
+            float maxHealth = character.CharacterHealth.MaximumHealth;
 
             float ratio = health / maxHealth;
 
             mainGroup.alpha = 1 - ratio;
         }
+
+        
+
         
             
     }
 
     public IEnumerator WaitForFindingPlayer()
     {
-        yield return new WaitForSeconds(1f);
-        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Character>();
+        while(character == null)
+        {
+            yield return new WaitForSeconds(1f);
+            FindCharacter();
+        }
+        
         canFind = true;
+
+        if (character != null) text.text = character.name;
+
+    }
+
+    public void FindCharacter()
+    {
+        var players = GameObject.FindGameObjectsWithTag("Player");
+
+        foreach (var player in players)
+        {
+            var _character = player.GetComponent<Character>();
+            if (_character != null)
+            {
+                character = _character;
+                break;
+            }
+        }
     }
 }
