51abb59eb46671eda6b07edd63069ee4250d72fe
Inutto <57230511+Inutto@users.noreply.github.com>
2022-03-27 00:20:54 -0600

    Update Oneway Trigger Visual

---
+++ Assets/_Scripts/FlipTrigger.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipTrigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private Collider2D _collider;
+    private SpriteRenderer _spriteRender;
+    private bool hasTriggered = false;
+
+    void Start()
+    {
+        _collider = GetComponent<Collider2D>();
+        _spriteRender = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!hasTriggered)
+        {
+            _spriteRender.flipX = true;
+            hasTriggered = true;
+        }
+    }
+}
